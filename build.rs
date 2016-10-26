073dd07b0b52f280e3b47f94fa9d03816b467ba3
Alexander Thaller <alexander.thaller@trivago.com>
2016-10-26 13:16:55 +0200

    Added autocompletion for commands and subcommands.

---
+++ build.rs
@@ -0,0 +1,15 @@
+// build.rs
+#![feature(custom_derive)]
+#[macro_use]
+extern crate clap;
+
+use clap::{App,Shell};
+
+fn main() {
+    let yaml = load_yaml!("src/cli.yml");
+    let mut app = App::from_yaml(yaml).version(crate_version!());
+
+    app.gen_completions("lablog", Shell::Bash, "autocomplete");
+    app.gen_completions("lablog", Shell::Fish, "autocomplete");
+    app.gen_completions("lablog", Shell::Zsh, "autocomplete");
+}
